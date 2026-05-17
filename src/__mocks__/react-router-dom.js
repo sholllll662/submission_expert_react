f52644218319ea4d0552c40dc986eedc3089e9bf
M_Sholahuddin <94447186+sholllll662@users.noreply.github.com>
2026-05-17 10:03:50 +0700

    fix: lint errors in mock router and threads test

--- src/__mocks__/react-router-dom.js
+++ src/__mocks__/react-router-dom.js
@@ -1,11 +1,9 @@
-import React from "react";
-
-export const BrowserRouter = ({ children }) => <>{children}</>;
-export const MemoryRouter = ({ children }) => <>{children}</>;
+export const BrowserRouter = ({ children }) => children;
+export const MemoryRouter = ({ children }) => children;
 export const Routes = () => null;
 export const Route = () => null;
-export const Link = ({ children }) => <>{children}</>;
-export const NavLink = ({ children }) => <>{children}</>;
+export const Link = ({ children }) => children;
+export const NavLink = ({ children }) => children;
 
 export const useNavigate = () => () => {};
 export const useLocation = () => ({ pathname: "/" });
